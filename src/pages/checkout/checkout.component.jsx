b45e5547eac061af50c04ce96eda6f9016c4343b
Hojun Bun <ponnponn.p@gmail.com>
2020-04-16 15:57:16 +0900

    updated checkout payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
 import './checkout.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from './../../redux/cart/cart.selectors';
 import CheckoutItem from './../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className="checkout-page">
@@ -33,6 +33,13 @@ const CheckoutPage = ({cartItems, total}) => (
         }
         <div className="total">
             <span>TOTAL: ${total}</span>
+        </div>
+        <StripeCheckoutButton price={total}/>
+        <div>
+            test card info<br />
+            4242 4242 4242 4242<br />
+            05/20<br />
+            123<br />
         </div>
     </div>
 );
