fc54df380d13ca767d80a8278fb51539c3b3751f
Hojun Bun <ponnponn.p@gmail.com>
2020-04-24 17:58:15 +0900

    created container

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,17 @@
+import { createStructuredSelector } from "reselect";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selector";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import CollectionPage from './collection.component';
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const mapStateToProps = createStructuredSelector({
+    isLoading: state => !selectIsCollectionFetching(state)
+});
+
+const CollectionPageContainer = compose(
+    connect(mapStateToProps),
+    WithSpinner
+)(CollectionPage);
+
+export default CollectionPageContainer;
